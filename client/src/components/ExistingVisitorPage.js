dbbcc4ae32188eab1cdd3713f11dc1163ccc6d39
Aryan Raj <aryanraj2k25@gmail.com>
2023-07-15 00:59:54 +0530

    sql query working

--- client/src/components/ExistingVisitorPage.js
+++ client/src/components/ExistingVisitorPage.js
@@ -1,28 +1,68 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
-import React from 'react'
-import Navbar from './Navbar'
-import { Link } from 'react-router-dom'
+import { Box, Button, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 const ExistingVisitorPage = () => {
+  const [visitorID, setVisitorID] = useState('');
+  const [purpose, setPurpose] = useState('');
+
+  const checkIn = () => {
+    Axios.post('http://localhost:3001/existingcheckin', { visitorID: visitorID, purpose: purpose })
+      .then(response => {
+        alert("Hurrah! Successfully checked in!!!!");
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const checkOut = () => {
+    Axios.post('http://localhost:3001/existingcheckout', { visitorID: visitorID, purpose: purpose })
+      .then(response => {
+       alert("Hurrah! Successfully checked out!!!!");
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     <div>
-        <Navbar />
-        <Typography variant="h2" align='center' >Existing Visitor Page</Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '400px', margin: '0 auto' }}>
-        <TextField id="outlined-basic" label="Visitor ID" variant="outlined" margin="normal" sx={{width:"90%"}}/>
-        <TextField id="outlined-basic" label="Purpose" variant="outlined" margin="normal" sx={{width:"90%"}} />
-       
+      <Navbar />
+      <Typography variant="h2" align="center">Existing Visitor Page</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '400px', margin: '0 auto' }}>
+        <TextField
+          id="visitor-id"
+          label="Visitor ID"
+          type="text"
+          onChange={(e) => setVisitorID(e.target.value)}
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "90%" }}
+        />
+        <TextField
+          id="purpose"
+          label="Purpose"
+          type="text"
+          onChange={(e) => setPurpose(e.target.value)}
+          variant="outlined"
+          margin="normal"
+          sx={{ width: "90%" }}
+        />
+
         <Box sx={{ display: 'flex', justifyContent: 'center', gap: '16px', marginTop: '16px' }}>
-        <Button variant="contained" component={Link} to="/checkedin">
-          Check In
-        </Button>
-        <Button variant="contained" component={Link} to="/checkedout">
-          Check Out
-        </Button>
-      </Box>
+          <Button onClick={checkIn} variant="contained" component={Link} to="/checkedin">
+            Check In
+          </Button>
+          <Button onClick={checkOut} variant="contained" component={Link} to="/checkedout">
+            Check Out
+          </Button>
+        </Box>
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default ExistingVisitorPage
+export default ExistingVisitorPage;
--- server/index.js
+++ server/index.js
@@ -17,4 +17,50 @@ app.get('/',(req,res)=>{
 }
 );
 
+//now we need to connect to the database
+const db=mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'password',
+    database:'kiosk',
+    insecureAuth : true
+}
+);
+
+//the database is now connected, we will add a test data to the server and see if it is working
+
+
+//the above line of code is used to insert data into the database
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//API to insert the new check in data into the database
+
+app.post('/newcheckin', (req, res) => {
+    const checkInDetails = req.body;
+  
+    const sqlInsert = 'INSERT INTO vms (firstName, lastName, email, phone, company, purpose) VALUES (?, ?, ?, ?, ?, ?)';
+    const values = [
+      checkInDetails.firstName,
+      checkInDetails.lastName,
+      checkInDetails.emailAddress,
+      checkInDetails.phoneNumber,
+      checkInDetails.companyName,
+      checkInDetails.purpose,
+    ];
+  
+    db.query(sqlInsert, values, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error inserting check-in data');
+      } else {
+        console.log('Check-in data inserted successfully');
+        res.status(200).send('Check-in data inserted successfully');
+      }
+    });
+  });
+
+
 
